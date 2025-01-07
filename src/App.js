40ebf5a494766cc71d6aa9d972ce6d95add1210e
Dakota <carmer95@gmail.com>
2025-01-07 11:29:35 -0500

    Added a Signin component with routing state for SPA feel, Also added Register, not fully built. Signin/Register forms are not active, but routing between home, signin, and register, is functioning.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import ParticlesBg from 'particles-bg';
 import './App.css';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
 
 
 class App extends Component {
@@ -15,6 +17,7 @@ class App extends Component {
 			input: '',
       imageURL: '',
       box: {},
+      route: 'signin'
 		}
 	}
 
@@ -23,7 +26,17 @@ class App extends Component {
     const image = document.getElementById('inputimage');
     const width = Number(image.width);
     const height = Number(image.height);
-    console.log(data, width, height, '41');
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box})
   }
 
   onInputChange = (event) => {
@@ -60,41 +73,54 @@ class App extends Component {
       .then((response) => response.json())
       .then((result) => {
         console.log(result);
-        if (result.outputs) {
-          calculateFaceLocation(result);
-          const regions = result.outputs[0].data.regions;
+        // if (result.outputs) {
+        //   const regions = result.outputs[0].data.regions;
   
-          regions.forEach((region) => {
-            const boundingBox = region.region_info.bounding_box;
-            const topRow = boundingBox.top_row.toFixed(3);
-            const leftCol = boundingBox.left_col.toFixed(3);
-            const bottomRow = boundingBox.bottom_row.toFixed(3);
-            const rightCol = boundingBox.right_col.toFixed(3);
+        //   regions.forEach((region) => {
+        //     const boundingBox = region.region_info.bounding_box;
+        //     const topRow = boundingBox.top_row.toFixed(3);
+        //     const leftCol = boundingBox.left_col.toFixed(3);
+        //     const bottomRow = boundingBox.bottom_row.toFixed(3);
+        //     const rightCol = boundingBox.right_col.toFixed(3);
   
-            region.data.concepts.forEach((concept) => {
-              const name = concept.name;
-              const value = concept.value.toFixed(4);
+        //     region.data.concepts.forEach((concept) => {
+        //       const name = concept.name;
+        //       const value = concept.value.toFixed(4);
   
-              console.log(
-                `${name}: ${value} BBox: ${topRow}, ${leftCol}, ${bottomRow}, ${rightCol}`
-              );
-            });
-          });
-        }
+        //       console.log(
+        //         `${name}: ${value} BBox: ${topRow}, ${leftCol}, ${bottomRow}, ${rightCol}`
+        //       );
+        //     ;
+        //     });
+        //   });
+        // }
+        this.displayFaceBox(this.calculateFaceLocation(result))
       })
       .catch((error) => console.log('error', error));
   };
   
+  onRouteChange = (route) => {
+    this.setState({route: route})
+  }
 
     render() {
       return (
         <div className="App">
           <ParticlesBg color="" num={130} type="cobweb" bg={true} />
-          <Navigation />
-          <Logo />
-          <Rank />
-          <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
-          <FaceRecognition imageURL={this.state.imageURL}/>
+          <Navigation onRouteChange={this.onRouteChange} />
+          { this.state.route === 'home'
+          ? <div>
+              <Logo />
+              <Rank />
+              <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+              <FaceRecognition box={this.state.box} imageURL={this.state.imageURL}/>
+            </div>
+          : (
+            this.state.route === 'signin'
+          ? <Signin onRouteChange={this.onRouteChange} />
+          : <Register onRouteChange={this.onRouteChange}/>
+          )
+          } 
         </div>
     );
   }
